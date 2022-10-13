e65e42351a84fa4224fe75d60eac37b274fa0d27
factism <factism001@gmail.com>
2022-10-13 15:05:00 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,12 @@
  * Return: 0
  */
 int main(void)
+	char = charType;
+	int = intType
+	long - int = longintType;
+	long - long int = longlong intType;
+	float = floatType;
 {
-	char charType;
-	int intType;
-	long int longType;
-	long long int longlong intType;
-	float floatType
-
 	printf("Size of a char: %c byte(s)\n", sizeof(charType));
 	printf("Size of an int: %i byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %li byte(s)\n", sizeof(longintType));
