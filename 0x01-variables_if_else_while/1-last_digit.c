593b963c2a63ebb16003a55c0e6a07650d7bc21b
factism <factism001@gmail.com>
2022-10-15 03:28:44 -0700

    last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 		printf("Last digit of %i is %d and is 0\n", n, lastd);
 	} else if (lastd < 6 && !0)
 	{
-		printf("Last digit of %i is %d and is less tgan 6 and not 0\n", n, lastd);
+		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
