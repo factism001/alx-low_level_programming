481af4c6d9377ce3b60730d80b2d261fb4947052
factism <factism001@gmail.com>
2022-10-15 07:45:31 -0700

    print base 10 nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+  * main - entry point
+  * description: print all single digit numbers of base 10
+  * Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("i%\n", n);
+	}
+
+	return (0);
+}
