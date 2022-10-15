fdf803e7aa5b1b0184788b8e4a1054d7567ba7c1
factism <factism001@gmail.com>
2022-10-15 05:41:58 -0700

    print alpha minus some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description: prints all lowercase alphabets except q and e
+ *Return: always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z', c != ('q' && 'e'))
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
