8155f601683231b8b804fdfab94d9f7d5ad4b3ff
factism <factism001@gmail.com>
2022-10-15 09:23:41 -0700

    print lowercase numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description: print all numbers of base 16 in lower case
+  * Return: always 0
+  */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < g; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
