f6058228bf12b1d87357595098752f7c79448c59
factism <factism001@gmail.com>
2022-10-15 04:37:15 -0700

    print lowercase a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Use `putchar` function to print the alphabet in lowercase.
+* Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n')
+
+	return (0);
+}
