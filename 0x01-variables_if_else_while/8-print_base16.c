15f85a45dc6f197ce19785128d4c64b1bfd6f5e4
factism <factism001@gmail.com>
2022-10-16 02:55:51 -0700

    print base 16 lowercase number

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,10 @@ int main(void)
 	char c;
 
 	for (i = 0; i <= 10; i++)
-		putchar(i + '0');
+		putchar((i % 10) + '0');
 
 	for (c = 'a'; c < 'g'; c++)
-		putchar(c, (i + '0'));
+		putchar(c);
 
 	putchar('\n');
 
