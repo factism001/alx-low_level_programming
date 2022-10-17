dfc266a5548e679185a09d910bcb38343b5a60c9
factism <factism001@gmail.com>
2022-10-17 03:42:55 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,14 @@ int main(void)
 	char letter;
 
 	for (num = 0; num <= 10; num++)
+	{
 		putchar(num + '0');
+	}
 
 	for (letter = 'a'; letter <= 'f'; letter++)
+	{
 		putchar(letter);
-
+	}
 	putchar('\n');
 
 	return (0);
