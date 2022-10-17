9372fe850cca7eef7af6039f45bf3b42f1862550
factism <factism001@gmail.com>
2022-10-17 03:21:35 -0700

    possible digits with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,12 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n);
 
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-
-	return (0);
+		return (0);
 }
