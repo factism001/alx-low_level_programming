4be92d612398adaea82b1037dfd6507ef0b1f65d
factism <factism001@gmail.com>
2022-10-18 09:07:48 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
   *print_alphabet - prints alphabets in loeercase followed by new line
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
