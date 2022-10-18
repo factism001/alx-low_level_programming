5800e96029aecdbfe758fc9a7cc62717762e844e
factism <factism001@gmail.com>
2022-10-18 05:09:56 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description: a program that prints _putchar
+  * Return: Always 0
+  */
+int main(void)
+{
+	char c = _putchar;
+	{
+		putchar(c);
+
+	}
+	return (0);
+}
