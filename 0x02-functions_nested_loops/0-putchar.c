1891355815c6a956f08406102e8474b821be99de
factism <factism001@gmail.com>
2022-10-18 08:30:09 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,15 @@
   */
 int main(void)
 {
-	char c = _putchar;
-	{
-		putchar(c);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	}
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * file: prototype of functions
+*/
+
+int _putchar(char c);
+
+#endif
