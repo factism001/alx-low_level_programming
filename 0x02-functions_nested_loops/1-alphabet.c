a5dda05bafeaaad6a4f34632f5def830233211b9
factism <factism001@gmail.com>
2022-10-18 08:59:32 -0700

    print alphabet in loeercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *print_alphabet - prints alphabets in loeercase followed by new line
+*/
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+		_putchar('\n');
+	}
+}
