c63b6f72cc8a163f5e83ad1cf5cd1df5009eb277
factism <factism001@gmail.com>
2022-10-19 07:42:47 -0700

    abolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _abs - Computes the absolute value of integer
+  * @n: The integer to be computed
+  * Return: n, -n
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
