727075c55f88be1d1a6bff4b648934b68a53ab79
factism <factism001@gmail.com>
2022-10-20 09:34:03 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_numbers - prints number from zero to nine with new line
+  * Return: 0
+  */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+
+	return (0);
+}
