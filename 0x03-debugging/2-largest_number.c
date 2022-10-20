118093c4b59bcea33561ee70884e430325b3d200
factism <factism001@gmail.com>
2022-10-20 04:34:40 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	largest = largest_number(a, b, c);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
 	return (largest);
 }
