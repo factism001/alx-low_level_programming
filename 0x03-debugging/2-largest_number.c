31ae0218f1486d9665d4920765b20e06d13637b5
factism <factism001@gmail.com>
2022-10-20 04:28:51 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
-	{
-		printf("%i is the largest number\n", a);
-	}
-	else if (b >= a && a >= c)
-	{
-		printf("%i is the largest number\n",     b);
-	}
-	else
-	{
-		printf("%i is the largest number\n",     c);
-	}
+	largest = largest_number(a, b, c);
 
 	return (largest);
 }
