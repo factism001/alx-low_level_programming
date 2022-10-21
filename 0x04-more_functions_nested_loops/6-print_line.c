3f7f94447f8826fbd04be9b41d67e6baef885cfe
factism <factism001@gmail.com>
2022-10-21 02:21:43 -0700

    print new line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,13 +5,13 @@ include "main.h"
   */
 void print_line(int n)
 {
-	if (n > 0;)
-	{
-		_putchar(95);
-	}
-	else if (n <=0)
-	{
-		_putchar('\n');
-	}
+	int line;
 
+	for (line = 0; line < n; line++)
+	{
+		if (n > 0)
+			_putchar(95);
+	}
+	else
+		_putchar('\n');
 }
