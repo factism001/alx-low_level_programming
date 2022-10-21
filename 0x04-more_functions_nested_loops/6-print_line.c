59072f68bcff9be339b63839ee65d88422de6e6d
factism <factism001@gmail.com>
2022-10-20 19:39:49 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+  * print_line - draws a straight line on terminal
+  * @n: number of times to draw line
+  */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
