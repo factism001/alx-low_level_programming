8eb8386ab7cafe458e6976e06a728088f4b1399c
factism <factism001@gmail.com>
2022-10-20 18:40:17 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,9 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 48; num <= 57; num++)
+	for (num = 48; num < 58; num++)
 	{
-		if num != 50 && num != 52
+		if (num != 50 && num != 52)
 			_putchar(num);
 	}
 	_putchar('\n');
