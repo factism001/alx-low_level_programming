5a377b8690f9671132ced52dcd495c740a134ea4
factism <factism001@gmail.com>
2022-10-20 19:12:41 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,11 @@ void more_numbers(void)
 	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
-			_putchar(num + '0');
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
