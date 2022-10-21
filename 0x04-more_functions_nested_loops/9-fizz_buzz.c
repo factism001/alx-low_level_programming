57129b3f7b8e0214fe9436cacf5da0fd9df7ad8c
factism <factism001@gmail.com>
2022-10-21 04:29:49 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,21 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (num % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else
 		{
-			printf(" %d", num);
+			printf("%d", num);
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
