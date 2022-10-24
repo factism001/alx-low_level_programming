ba17fe679025c166d6ac94a6ef125c004ef7e13d
factism <factism001@gmail.com>
2022-10-24 07:47:44 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _strlen - returns the length of a string.
+  * @s: input string.
+  * Return: lenght of string
+  */
+int _strlen(char *s)
+{
+	int lenght;
+	int *s;
+
+		lenght = _strlen(*s);
+
+		return (lenght);
+}
