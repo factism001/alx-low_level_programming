0d46752a845339632475cdcfa463d92be94b964f
factism <factism001@gmail.com>
2022-10-24 07:02:12 -0700

    update with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#inclide "main.h"
+
+/**
+  * reset_to_98 - function that takes a pointer to an int as parameter
+  * and updates the value it points to to 98.
+  * @n: variable integerto update.
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
