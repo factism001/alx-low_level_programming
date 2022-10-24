dda2cc98fddcdec16f88c71caac86bbb7c1d38ef
factism <factism001@gmail.com>
2022-10-24 08:48:20 -0700

    string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,16 @@
   */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int count = 0;
 
-	while (s[index++])
-		len++;
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 	_putchar('\n');
 }
