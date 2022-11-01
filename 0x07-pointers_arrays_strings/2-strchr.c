cd2cb60fc170afc12751ba958fe68c8454a67c3f
factism <factism001@gmail.com>
2022-11-01 05:27:50 -0700

    locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,9 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	for (i = 0; *(s + i) != '\0'; i++)
-	{
 		if (*(s + i) == c)
 			return (s + i);
-
-		return ('\0');
+	if (*(s + i ) == c)
+		return (s + i);
+	return ('\0');
 }
