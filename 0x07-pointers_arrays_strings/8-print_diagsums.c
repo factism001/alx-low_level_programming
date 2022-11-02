e6ef84324fce0381e9ca780dc83f5f5cb58fcc25
factism <factism001@gmail.com>
2022-11-02 09:59:11 -0700

    square matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  * print_diagsums - prints the sum of the two diagonals of a square matrix.
+  * @a: the arraybto be checked
+  * @size: size of the array to be checked
+  * Return: no return
+  */
+void print_diagsums(int *a, int size)
+{}
