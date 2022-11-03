c7db506ea4254c9b1a5d7384123931e3ea455517
factism <factism001@gmail.com>
2022-11-03 06:11:30 -0700

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - a functiin that prints string in reverse
+  * @s: the string to print
+  * return: no return
+  */
+void _print_rev_recursion(char *s);
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
