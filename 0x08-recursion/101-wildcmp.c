4fbe04fc49a591403f0037ed22640476d6fca48d
factism <factism001@gmail.com>
2022-11-04 03:58:53 -0700

    compare string

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * wildcmp - compares if two strings are identical
+  * @s1: first string
+  * @s2: second string
+  * Return: 1 if they are identical and 0 if not
+  */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (1);
+	else
+		return (0);
+}
