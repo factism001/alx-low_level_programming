7c3de3390b96194c0729cc5c38764a3eab6459f8
factism <factism001@gmail.com>
2022-11-04 05:54:55 -0700

    prints the number of arguements passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - a program that prints the number of
+  * arguements passed into it
+  * @argc: the number of command line arguement used
+  * @argv: the array containig the command line strings
+  * Return: 0 - sucess
+  */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
