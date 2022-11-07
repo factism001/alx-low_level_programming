5aedbe437f2490c5917bde71b99a7529c08c0c22
factism <factism001@gmail.com>
2022-11-07 04:34:58 -0800

    minimum change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+  * main - a program that prints the minimum number of coins to make change
+  * @argc: number of command line arguement
+  * @argv: array of arguments passed in program
+  * Return: 0 - success
+  */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int num_coin = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+
+		num_coin += 1
+
+	}
+	printf("%d\n", num_coin);
+	return (0);
+}
