cb1d7a1b1731a8a92396a3f5ec91f6985d0f6030
factism <factism001@gmail.com>
2022-11-08 03:31:57 -0800

    make a string duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _strdup - returns a pointer to a newly allocated space in memory,
+  * which contains a copy of the string given as a parameter.
+  * @str: the string to duplicate
+  * Return: a pointer to duplicated string
+  */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	ptr = malloc(sizeof(*ptr) * i + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		ptr[j] = str[i];
+
+	return (ptr);
+
+}
