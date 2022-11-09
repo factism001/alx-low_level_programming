3beeda0ba7afbe07f32a8c4070902a6baa9765bd
factism <factism001@gmail.com>
2022-11-09 03:29:28 -0800

    concatenate

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,7 +13,7 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (i = 0; total = 0; i < ac; i++)
 	{
 		if (str == NULL)
 			return (NULL);
