91b4ce61b94b54d61166fb3c044fbd1d06eeff0b
factism <factism001@gmail.com>
2022-11-10 03:30:45 -0800

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * malloc_checked - Allocates memory and causes normal process termination
+  * if function fails
+  * @b: number 8of spaces needed in memory
+  * Return: pointer to memory allocated
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(*ptr) * b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
