9f8951955c619d4365b8c7ad72487226d57e9dcb
factism <factism001@gmail.com>
2022-11-12 07:30:28 -0800

    multiply digits

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  * mul - function that multiplies two numbers
+  * @num1: the first number
+  * @num2: the second number
+  * Return: pointer to the function
+  */
+int mul(int num1, int num2)
+{
+	int *ptr;
+	int n;
+
+	if (!isdigit(num1 && num2))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n = 2;
+
+	ptr = malloc(2 * sizeof(*ptr));
+
+	if (n < 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (num1 >= 0; num1 < 9; num1++)
+		;
+	for (num2 >= 0; num2 < 9; num2++)
+		;
+	ptr = mul = num1 * num2;
+
+	printf("%d\n", ptr);
+
+	return (mul);
+}
