4e3dd92c4d4b7aa466dc56fcbe9361a2860488ba
factism <factism001@gmail.com>
2022-11-14 07:33:57 -0800

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,16 @@
 #define _DOG_H
 
 /**
-  * my_dog - typedef for struct dog
-  */
-typedef struct dog my_dog;
-
-/**
   * struct dog - structure that stores information on dogs
   * @name: name of the dog
   * @age: age of the dog
   * @owner: name of the dog owner
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char owner;
-};
+} my_dog;
 
 #endif
