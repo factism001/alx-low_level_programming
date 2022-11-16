92b83647d3562886838d17a398efe0d870d9b1bb
factism <factism001@gmail.com>
2022-11-16 03:06:44 -0800

    compare values

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+  * int_index - a function that searches for an integer with functiin pointers
+  * @array: tye array of elements
+  * @size: the size of array
+  * @cmp: the function pointer
+  * Return: index of first element tgat matches 'cmp' or '-1' of not found
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		return (i);
+	}
+
+	return (-1);
+}
