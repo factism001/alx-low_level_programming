58bfb7c9f7c21887bc70865d72bf22d54b0ad3b0
factism <factism001@gmail.com>
2022-11-16 03:23:28 -0800

    compare values

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	unsigned int i;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (cmp && array)
 	{
-		cmp(array[i]);
-		return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			return (i);
+		}
 	}
 
 	return (-1);
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
