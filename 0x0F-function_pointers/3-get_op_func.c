d02b8d05a695085af98b606ce94084736d9c5468
factism <factism001@gmail.com>
2022-11-17 01:11:13 -0800

    hotfox

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
   * get_op_func - selects the correct function to perform.
   * @s: the operator passed
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+
 /**
   * op_add - adds two numbers.
   * @a: first number.
