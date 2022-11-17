642b3770f02b6678b1ecad1cdd06aec7a24bccc6
factism <factism001@gmail.com>
2022-11-17 04:03:23 -0800

    print variadic string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 
 		if (seperator && i < n - 1)
-			printf("%s", separator)
+			printf("%s", separator);
 	}
 
 	printf("\n");
