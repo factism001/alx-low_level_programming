8545f3cee27d8e0340ad18f6957e3808d2ffae45
factism <factism001@gmail.com>
2022-11-24 04:29:32 -0800

    print elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t num_elem;
+	size_t nelem;
 
-	num_elem = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)")
-					else
-						printf("[%d] %s\n", h->len, h->str);
-					h = h->next;
-					num_elem++;
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num_elem++;
 	}
 	return (num_elem);
 }
