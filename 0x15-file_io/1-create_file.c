60a05149b4c963863822f91e8bac67bfb78bea9f
factism <factism001@gmail.com>
2022-12-05 07:47:47 -0800

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+	
 	if (!text_content)
 		text_content = "";
 
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
+	
 	wr = write(fd, text_content, num_letters);
 
 	if (wr == -1)
