aef2c95e633124d27f56b140aa7a6489527f3513
factism <factism001@gmail.com>
2022-12-05 07:43:05 -0800

    fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, num_letters, wr;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
