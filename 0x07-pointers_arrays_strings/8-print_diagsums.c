2f353e307a7ecf3d22475e419b1cd28e72ecf253
factism <factism001@gmail.com>
2022-12-21 01:42:26 -0800

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,9 +8,10 @@
   */
 void print_diagsums(int *a, int size)
 {
+	int i;
 	int sum1 = 0, sum2 = 0;
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - i - 1)];
