dc278a8a69948d36a8f5507b3e290520a725f6fa
factism <factism001@gmail.com>
2022-12-26 04:01:39 -0800

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,36 @@
 #include "main.h"
 
 /**
- * is_prime_number - returns 1 if the input integer
+ *is_prime_number_recursive - returns 1 if the input integer
  * is a prime number, and 0 if it is not
+ * is_prime_number - detects if number is prime number
  * @n: integer to check
+ * @i: integer to check for divisibility
+ *
  * Return: 1 if n is a prime number, 0 if it is not
  */
-int is_prime_number(int n)
+int is_prime_number_recursive(int n, int i)
 {
-	int i = 2;
-
 	if (n <= 1)
+	{
+		return (0);
+	} else if (i > n / 2)
+	{
+		return (1);
+	} else if (n % i == 0)
 	{
 		return (0);
 	}
 
-	while (i <= n / 2)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-		i++;
-	}
+	return (is_prime_number_recursive(n, i + 1));
+}
 
-	return (1);
+/**
+  * is_prime_number - detects if number is prime number
+  * @n: input number.
+  * Return: 1 if n is a prime number. 0 if n is not a prime number.
+  */
+int is_prime_number(int n)
+{
+	return (is_prime_number_recursive(n, 2));
 }
