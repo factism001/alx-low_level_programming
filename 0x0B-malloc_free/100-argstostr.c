18d21cfb10b4c65c1cc1d8edb0dd3fec70e7f27a
factism <factism001@gmail.com>
2022-12-27 02:34:24 -0800

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,25 +8,43 @@
   */
 char *argstostr(int ac, char **av)
 {
-	int i, j, total;
-	char *str, *new_string;
-
-	total = 0;
+	char *str;
+	int total_length, offset, length;
+	int i, j;
 
 	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
+	}
 
+	total_length = 0;
 	for (i = 0; i < ac; i++)
 	{
-		if (str == NULL)
-			return (NULL);
-		str = malloc(total * sizeof(str) + 1);
+		length = 0;
+
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			length++;
+		}
+		total_length += lenght + 1;
 	}
-	total++;
+	str = malloc(total_length + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	offset = 0;
+	for (i = 0; i < ac; i++)
+	{
+		length = 0;
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[offset++] = av[i][j];
+			length++;
+		}
+		str[offset++] = '\n';
+	}
+	str[offset] = '\0';
 
-	new_string = str;
-
-	for (j = 0; j != '\0'; j++)
-		av[i][j] = *str;
-	return (new_string);
+	return (str);
 }
