03ca6fee8c1b41558ec0fbb5e85dc47c9ae82bec
factism <factism001@gmail.com>
2022-12-29 02:02:19 -0800

    update

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - a function that prints anything
+ * @format: a list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int i = 0;
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f')
+		{
+			printf("%c", va_arg(args, int));
+		} else if (format[i] == 's')
+		{
+			char *s = va_arg(args, char*);
+
+			if (s == NULL)
+			{
+				printf("(nil)");
+			} else
+			{
+				printf("%s", s);
+			}
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
