4e97a84b28d944a174be735bb8b5df7fe23307be
factism <factism001@gmail.com>
2022-12-29 02:14:02 -0800

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,16 @@
-	  global   main
-	  extern   printf
+section .data
+    msg db 'Hello, Holberton', 10
+    len equ $ - msg
 
-	  section  .text
-main:	  
-	  mov	   rdi, message
-	  call     printf
-	  ret
-message:  
-          db       "Hello World", 0
+section .text
+    global _start
+
+_start:
+    mov rdi, msg
+    mov rsi, len
+    xor rax, rax
+    call printf
+    xor rax, rax
+    mov rdi, 0
+    call exit
+
